7b2b49654cc394ff1804260b7aa826a8c58d2139
StandardGage <gagedolph@yahoo.com>
2023-04-29 15:13:43 -0400

    add learn logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Landing from './components/Landing'
 import NavBar from './components/NavBar'
 import { BiUserCircle } from "react-icons/bi"
+import { FaGraduationCap } from "react-icons/fa"
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
           <div>Logo</div>
           <div className=''>Blue Hen Shuffle</div>
           <div className='flex space-x-2'>
-            <button className='justify-end'>Learn</button>
+            <button className='flex justify-center items'><FaGraduationCap size={30}></FaGraduationCap>Learn</button>
             <button className='flex justify-center self-end'><BiUserCircle size={30}></BiUserCircle>Login</button>
           </div>
         </ul>
