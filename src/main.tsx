5435a03e47aadd86dae37cc54ee61befa62be48f
StandardGage <50509562+StandardGage@users.noreply.github.com>
2023-04-29 19:35:29 -0400

    added routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  HashRouter,
+} from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
 )
