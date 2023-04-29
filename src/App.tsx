43dbe5e985662fac37b1b4edae267a6f6f9e66d7
StandardGage <gagedolph@yahoo.com>
2023-04-29 15:17:47 -0400

    added react-icons, added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Landing from './components/Landing'
+import NavBar from './components/NavBar'
+import { BiUserCircle } from "react-icons/bi"
 
 function App() {
 
@@ -7,6 +9,18 @@ function App() {
     <>
       <Landing></Landing>
     </>
+    <div>
+      <NavBar>
+        <ul className='flex justify-around'>
+          <div>Logo</div>
+          <div className=''>Blue Hen Shuffle</div>
+          <div className='flex space-x-2'>
+            <button className='justify-end'>Learn</button>
+            <button className='flex justify-center self-end'><BiUserCircle size={30}></BiUserCircle>Login</button>
+          </div>
+        </ul>
+      </NavBar>
+    </div>
   )
 }
 
