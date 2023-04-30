927a6228723535eb464f4727c6e610209e09e659
StandardGage <50509562+StandardGage@users.noreply.github.com>
2023-04-29 20:12:20 -0400

    move logo

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,8 @@ function App() {
   return (
     <div>
       <NavBar>
-      <Link to="/" className='flex text-4xl justify-center'>Chirp</Link>
         <ul className='flex flex-1 justify-between'>
-          <div>Logo</div>
+        <Link to="/" className='flex text-4xl justify-center'>Chirp</Link>
           <div className=' flex space-x-2'>
             <Link to="/learn" className='flex place-items-center p-2 hover:bg-udblue rounded-lg transition-all duration-300'><FaGraduationCap size={30}></FaGraduationCap>Learn</Link>
             <button className='flex place-items-center p-2 hover:bg-udblue rounded-lg transition-all duration-300'><BiUserCircle size={30}></BiUserCircle>Login</button>
