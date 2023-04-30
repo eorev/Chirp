30f3500a193e858ba977662750d07b8deaa4a832
Daniel DeFlores <ddeflores2@gmail.com>
2023-04-29 22:48:30 -0400

    add selection sort comparisons

--- src/pages/Level2.tsx
+++ src/pages/Level2.tsx
@@ -1,10 +1,29 @@
 
+import { useState } from "react";
 import { Draggable } from "react-drag-reorder";
 
+function arraysEqual(a: number[], b: number[]) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+  
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) return false;
+    }
+  
+    return true;
+  }
+
 export default function Level1() {
-    let state = {
-        array: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    };
+    const [array, setArray] = useState([2, 1, 4, 3, 5]);
+
+    const handleItemReorder = (currentPos:number, newPos:number) => {
+        const newArray = array.slice();
+        const [removed] = newArray.splice(currentPos, 1);
+        newArray.splice(newPos, 0, removed);
+        setArray(newArray);
+        console.log(newArray); // Log the updated array
+      };
 
     return (
         <div className="flex flex-col mt-24 justify-center items-center text-4xl">
@@ -16,6 +35,7 @@ export default function Level1() {
                 <li>Imagine you have a bunch of cards with numbers on them.</li>
                 <li>With selection sort, we first look at all of the cards and find the one with the lowest number.</li>
                 <li>We swap this card with the very first card, and then find the card with the smallest number again. This time though, don't include the card you had just found!</li>
+                <li>If the card you are going to swap with already has the lowest number, move to the next one!</li>
                 <li>Repeat this over and over, swapping the card you find with the second card, and then the third, and so on, until there are no more cards to swap.</li>
                 <li>Now you are done! You just used selection sort!</li>
             </ol>
@@ -26,12 +46,17 @@ export default function Level1() {
             </div>
 
             <div className="flex">
-                <Draggable>
-                    {state.array.map((item: any) => (
-                        <div className="w-10 h-10 bg-red-500 m-2">{item}</div>
+                <Draggable onPosChange={handleItemReorder}>
+                    {array.map((item) => (
+                    <div key={item} className="w-10 h-10 bg-red-500 m-2">
+                        {item}
+                    </div>
                     ))}
                 </Draggable>
             </div>
+            {arraysEqual(array, [1,2,4,3,5]) && <p className="text-sm">Start by  finding the smallest card and swapping it with the first card.</p>}
+            {arraysEqual(array, [1,2,3,4,5]) && <p className="text-sm">Great Job, now repeat the comparisons and swap the cards!</p>}
+            {arraysEqual(array, [1,2,3,4,5]) && <p className="text-sm">Great Job, You completed Selection Sort!</p>}
     </div>
     )
 }
