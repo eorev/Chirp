47ea954b7ff540fabf9393deeb452863ef759082
StandardGage <50509562+StandardGage@users.noreply.github.com>
2023-04-29 22:54:36 -0400

    updated selection sort

--- src/pages/Level2.tsx
+++ src/pages/Level2.tsx
@@ -54,8 +54,8 @@ export default function Level1() {
                     ))}
                 </Draggable>
             </div>
-            {arraysEqual(array, [1,2,4,3,5]) && <p className="text-sm">Start by  finding the smallest card and swapping it with the first card.</p>}
-            {arraysEqual(array, [1,2,3,4,5]) && <p className="text-sm">Great Job, now repeat the comparisons and swap the cards!</p>}
+            {arraysEqual(array, [2,1,4,3,5]) && <p className="text-sm">Start by  finding the smallest card and swapping it with the first card.</p>}
+            {arraysEqual(array, [1,2,4,3,5]) && <p className="text-sm">Great Job, now repeat the comparisons and swap the cards!</p>}
             {arraysEqual(array, [1,2,3,4,5]) && <p className="text-sm">Great Job, You completed Selection Sort!</p>}
     </div>
     )
