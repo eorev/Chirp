9dab70259dcb1e0c282c7761f5d45d6eaf68095a
StandardGage <50509562+StandardGage@users.noreply.github.com>
2023-04-29 23:01:37 -0400

    fix learn now button in landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './Landing.css'
 
 const Landing = () => {
@@ -8,9 +9,9 @@ const Landing = () => {
                     <h1 className='header-title'>Chirp</h1>
                     <h2 className='header-subtitle'>Learning Site by Students.</h2>
                     <h2 className='header-subtitle'>For Students.</h2>
-                    <button className='header-button hover:brightness-110'>
+                    <Link to='/learn' className='header-button hover:brightness-110'>
                         <h1>Learn Now</h1>
-                    </button>
+                    </Link>
                 </div>
                 <div className='header-right__container'>
                     <img src="https://picsum.photos/500" alt="" />
